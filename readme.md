e83fb00928bcaf1e7385126f6124e88379a13006
Harison Rios de Miranda <hharison562@gmail.com>
2024-09-29 15:22:23 -0300

    primeiro commit

---
+++ readme.md
@@ -0,0 +1,47 @@
+# 🧑‍💻 Conversor Numérico (Binário, Octal, Hexadecimal e Decimal)
+
+![Windows XP](assets/img/background.gif)
+
+Este projeto é um conversor de números entre diferentes bases (binário, octal, hexadecimal e decimal), com uma interface estilizada ao estilo do Windows XP. 
+
+
+
+## 🌐 Deploy 
+Clique aqui para acessar o site: [Conversor Numérico](https://conversorn.vercel.app/)
+
+
+## ⚠️ Aviso Importante
+
+Este conversor numérico **não é responsivo**, ou seja, ele foi projetado exclusivamente para ser utilizado em navegadores web no **desktop**. O layout e as funcionalidades podem não ser exibidos corretamente em dispositivos móveis, tablets ou telas menores.
+
+Para garantir a melhor experiência, acesse o site em um computador com um navegador de desktop.
+
+
+
+## 🚀 Funcionalidades
+- Conversão entre bases numéricas: binário, octal, hexadecimal e decimal.
+- Interface nostálgica baseada no tema do Windows XP.
+- Exibição em tempo real dos resultados.
+
+## 💻 Instalação
+
+1. Clone o repositório:
+    ```bash
+    git clone https://github.com/HarisonRios/ConversorNumerico
+    ```
+
+2. Abra o arquivo `index.html` no navegador.
+
+## 📖 Uso
+
+1. Escolha a base numérica que deseja converter no seletor.
+2. Insira o valor no campo de entrada.
+3. Visualize os resultados convertidos nas outras bases numéricas automaticamente.
+
+## 🤝 Contribuidores
+
+ - Harison Rios [https://github.com/HarisonRios]
+ - João Victor Galdino [https://github.com/JoaoGaldinoCunha]
+
+---
+**Nota:** Este projeto foi feito para fins educacionais e nostálgicos - Materia de Arquitetura Computacional.
---
+++ script.js
@@ -0,0 +1,149 @@
+function CalcularaHexaPara(){
+    exibir.innerHTML = " ";
+    var valorHexadecimal = valorDoHexa.value;
+    var tipoDoValorBase = escolha_tipo_hexa_para.value;
+    var hexaconverter = parseInt(valorHexadecimal, 16);
+
+    var decimal = hexaconverter.toString(10);
+    var binario = hexaconverter.toString(2);
+    var octal = hexaconverter.toString(8);
+
+    if (tipoDoValorBase == "hexadecimal_para_decimal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${decimal}`;
+    } else if (tipoDoValorBase == "hexadecimal_para_binario") {
+        exibir.innerHTML = `O valor convertido foi: <br>${binario}`;
+    } else if (tipoDoValorBase == "hexadecimal_para_octal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${octal}`;
+    } else {
+        exibir.innerHTML = `Valores incompatíveis, favor selecionar algo e digitar um valor em Hexadecimal.`;
+    }
+}
+
+function CalcularDecimalPara() {
+    exibir.innerHTML = " ";
+    var valorDecimal = Number(valorDoDecimal.value);
+    var tipoDoValorBase = escolha_tipo_decimal_para.value;
+
+    var hexadecimal = valorDecimal.toString(16);
+    var binario = valorDecimal.toString(2);
+    var octal = valorDecimal.toString(8);
+
+    if (tipoDoValorBase == "decimal_para_binario") {
+        exibir.innerHTML = `O valor convertido foi: <br>${binario}`;
+    } else if (tipoDoValorBase == "decimal_para_octal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${octal}`;
+    } else if (tipoDoValorBase == "decimal_para_hexadecimal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${hexadecimal}`;
+    } else {
+        exibir.innerHTML = `Valores incompatíveis, favor selecionar algo e digitar um valor decimal.`;
+    }
+}
+
+function CalcularOctalPara() {
+    exibir.innerHTML = " ";
+    var valorOctal = valorDoOctal.value;
+    var tipoDoValorBase = escolha_tipo_octal_para.value;
+    var octalconverter = parseInt(valorOctal, 8);
+
+    var hexadecimal = octalconverter.toString(16);
+    var decimal = octalconverter.toString(10);
+    var binario = octalconverter.toString(2);
+
+    if (tipoDoValorBase == "octal_para_decimal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${decimal}`;
+    } else if (tipoDoValorBase == "octal_para_binario") {
+        exibir.innerHTML = `O valor convertido foi: <br>${binario}`;
+    } else if (tipoDoValorBase == "octal_para_hexadecimal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${hexadecimal}`;
+    } else {
+        exibir.innerHTML = `Valores incompatíveis, favor selecionar algo e digitar um valor octal.`;
+    }
+}
+
+function CalcularBinarioPara() {
+    exibir.innerHTML = " ";
+    var valorBinario = valorDoBinario.value;
+    var tipoDoValorBase = escolha_tipo_binario_para.value;
+    var binarioconverter = parseInt(valorBinario, 2);
+
+    var hexadecimal = binarioconverter.toString(16);
+    var decimal = binarioconverter.toString(10);
+    var octal = binarioconverter.toString(8);
+
+    if (tipoDoValorBase == "binario_para_decimal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${decimal}`;
+    } else if (tipoDoValorBase == "binario_para_octal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${octal}`;
+    } else if (tipoDoValorBase == "binario_para_hexadecimal") {
+        exibir.innerHTML = `O valor convertido foi: <br>${hexadecimal}`;
+    } else {
+        exibir.innerHTML = `Valores incompatíveis, favor selecionar algo e digitar um valor binário.`;
+    }
+}
+
+function converterCaracterAscii() {
+
+    div_output.innerHTML = '';
+
+    var caractere = input_ascii.value;
+    var decimal = caractere.charCodeAt();
+
+    div_output.style.display = 'block';
+
+    div_output.innerHTML += "<p>Decimal: " + decimal + '</p><br>';
+    div_output.innerHTML += "<p>Binário: " + decimal.toString(2) + '</p><br>';
+    div_output.innerHTML += "<p>Hexadecimal: " + decimal.toString(16) + '</p><br>';
+    div_output.innerHTML += "<p>Octal: " + decimal.toString(8) + '</p><br>';
+}
+
+
+
+// CODIGOS EXTERNOS
+
+// PRELOADER
+
+window.addEventListener('load', () => {
+    const minDelay = 1000; 
+    
+    const preloader = document.getElementById('preloader');
+    const content = document.getElementById('content');
+
+    
+    const startTime = Date.now();
+    
+    
+    function hidePreloader() {
+        const elapsedTime = Date.now() - startTime;
+        const delay = Math.max(minDelay - elapsedTime, 0); 
+        
+        setTimeout(() => {
+            preloader.style.opacity = '0';
+            preloader.style.visibility = 'hidden';
+            content.style.display = 'block';
+        }, delay);
+    }
+    
+    hidePreloader();
+});
+
+
+// NAVBAR
+
+window.onload = function () {
+    const currentPage = window.location.pathname;
+
+    const navLinks = document.querySelectorAll('.navbar li a');
+
+    navLinks.forEach(link => {
+        const linkPath = link.getAttribute('href');
+
+
+        if ((currentPage === '/index.html' || currentPage === '/') && linkPath === '../index.html') {
+            link.classList.add('active');
+        }
+
+        else if (linkPath === currentPage) {
+            link.classList.add('active');
+        }
+    });
+};
